08c392655b444c752d6e2d65fad0dcc79ceaeb60
fenshitianyue <abzanda@163.com>
2019-05-26 12:30:16 +0800

    add http module code

---
+++ server_http.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "server_base.hpp"
+
+namespace WebServer{
+  typedef boost::asio::ip::tcp::socket HTTP;
+  template<>
+  class Server<HTTP> : public ServerBase<HTTP> {
+  public:
+    Server(unsigned short port, size_t num_threads = 1) : ServerBase<HTTP>::ServerBase(port, num_threads){};
+  private:
+    //实现基类ServerBase的虚函数accept
+    void accept(){
+      auto socket = std::make_shared<HTTP>(m_io_service);
+      acceptor.async_accept(*socket, 
+                            [this, socket](const boost::system::error_code& ec){
+                              accept();
+                              if(!ec){
+                                process_request_and_respond(socket);
+                              }
+                            });
+    }
+  };
+}
+
+
