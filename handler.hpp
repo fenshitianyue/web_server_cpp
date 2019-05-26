9d05fb11e9f9bfd92157b7abb60f3155861e994c
fenshitianyue <abzanda@163.com>
2019-05-26 12:41:47 +0800

    add code to intercat with the HTTP framework

---
+++ handler.hpp
@@ -0,0 +1,10 @@
+#include <fstream>
+#include "server_base.hpp"
+
+using namespace std;
+using namespace WebServer;
+
+template<typename SERVER_TYPE>
+void start_server(SERVER_TYPE& server){
+  //TODO
+}
