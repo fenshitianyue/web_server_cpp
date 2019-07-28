394c54403f429dc14def7c11e1c1cc7a63af025e
fenshitianyue <abzanda@163.com>
2019-07-28 15:56:04 +0800

    add web logic code

--- handler.hpp
+++ handler.hpp
@@ -1,10 +1,17 @@
-#include <fstream>
 #include "server_base.hpp"
-
-using namespace std;
-using namespace WebServer;
+#include <fstream>
 
 template<typename SERVER_TYPE>
 void start_server(SERVER_TYPE& server){
-  //TODO
+  //向服务器添加请求资源的方法
+  //1.处理默认 GET 请求，如果没有其他匹配成功，则这个函数将应答wwwroot/目录下的index.html文件
+  server.default_resource["^/?(.*)$"]["GET"] = [](std::ostream& response, WebServer::Request& request){
+    std::string filename = "wwwroot/";
+    std::string path = request.path_match[1];
+    //防止客户端使用'..'来访问wwwroot/目录之外的内容
+    size_t last_pos = path.rfind(".");
+    size_t current_pos = 0;
+    size_t pos = 0;
+  };
+
 }
